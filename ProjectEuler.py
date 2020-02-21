689179a706fa729cc020f68dcf73f6da519138e3
raphaellndr <54632013+raphaellndr@users.noreply.github.com>
2020-02-21 10:48:24 +0100

    Exercices 15 to 17

--- ProjectEuler.py
+++ ProjectEuler.py
@@ -1,4 +1,5 @@
 import math, time
+from math import *
 
 ############ EXERCICE 1 ############
 def multiples_of_3_and_5() :
@@ -196,8 +197,52 @@ def longest_Collatz_sequence(n) :
             #print(largest_length, longest_collatz_sequence)
     return longest_collatz_sequence
 
+############ EXERCICE 15 ############
+def lattice_paths(grid_size,k) :
+    return factorial(2*grid_size) / (factorial(k) * factorial(2*grid_size - k))
 
+############ EXERCICE 16 ############
+def power_digit_sum(number, power) :
+    number_at_power = number**power
+    number_list = [int(d) for d in str(number_at_power)]
+    digit_sum = 0
+    for i in number_list :
+        digit_sum += i
+    return digit_sum
 
+############ EXERCICE 17 ############
+def number_letter_counts(n) :
+    units = ['one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten', 'eleven', 'twelve',
+              'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen']
+    tens = ['twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety']
+    others = ['hundred', 'thousand', 'and']
+    count = 0
+    for i in range(1, n+1) :
+        number = [int(d) for d in str(i)]
+        if len(number) == 4 : count += len(others[1]) + len(units[0])
+        if len(number) == 3 :
+            count += len(units[number[0]-1]) + len(others[0])
+            if i % 100 != 0 : count += len(others[2])
+            if number[1] == 0 and i % 100 != 0 :
+                count += len(units[number[2]-1])
+            if number[1] == 1 :
+                count += len(units[number[2]+9])
+            if number[1] > 1 :
+                count += len(tens[number[1]-2])
+                if number[-1] != 0 :
+                    count += len(units[number[2]-1])
+        if len(number) == 2  :
+            if i < 20 :
+                count += len(units[i-1])
+            else :
+                count += len(tens[number[0]-2])
+                if number[-1] != 0 :
+                    count += len(units[number[1]-1])
+        if len(number) == 1 :
+            count += len(units[i-1])
+    return count
+
+############ EXERCICE 18 ############
 
 
 
