bcefc78a986f9cf965a38535ece37754b859b53d
raphaellndr <54632013+raphaellndr@users.noreply.github.com>
2020-02-25 23:03:44 +0100

    Cleaner code

--- Exercice01.py
+++ Exercice01.py
@@ -1,6 +1,23 @@
 ############ EXERCICE 1 ############
-def multiples_of_3_and_5() :
+
+"""
+
+If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
+
+Find the sum of all the multiples of 3 or 5 below 1000.
+
+"""
+
+
+def multiples_of_3_and_5(number):
+    """
+    Function that return the sum of all the multiples of 3 or 5 below 1000
+
+    :param number: the highest number that can be a multiple of 3 or 5
+    :return:
+    """
     sum_of_multiples = 0
-    for i in range(1000) :
-        if i % 3 == 0 or i % 5 == 0 : sum_of_multiples += i
+    for i in range(1, number):
+        if i % 3 == 0 or i % 5 == 0:
+            sum_of_multiples += i
     return sum_of_multiples
