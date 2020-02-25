4b431712c7ced11fc125f4b8958e2867831447ab
raphaellndr <54632013+raphaellndr@users.noreply.github.com>
2020-02-25 23:31:05 +0100

    Changes

--- Exercice04.py
+++ Exercice04.py
@@ -1,11 +1,28 @@
 ############ EXERCICE 4 ############
-def largest_palindrome() :
+
+
+"""
+
+A palindromic number reads the same both ways. The largest palindrome made
+from the product of two 2-digit numbers is 9009 = 91 × 99.
+
+Find the largest palindrome made from the product of two 3-digit numbers.
+
+"""
+
+
+def largest_palindrome():
+    """
+    Function that return the largest palindromic number that the product of
+    2 3-digit numbers can make.
+    :return:
+    """
     largest_palindrome = 0
-    for j in range(100,999) :
-        for i in range(100,999) :
+    for j in range(100, 999):
+        for i in range(100, 999):
             product = i * j
             a = [int(d) for d in str(product)]
             b = a[::-1]
-            if a == b :
+            if a == b:
                 largest_palindrome = product
     return largest_palindrome
--- Exercice05.py
+++ Exercice05.py
@@ -1,10 +1,31 @@
 ############ EXERCICE 5 ############
-def smallest_number(n) :
+
+
+"""
+
+2520 is the smallest number that can be divided by each of the numbers from 1
+to 10 without any remainder.
+
+What is the smallest positive number that is evenly divisible by all of the
+numbers from 1 to 20?
+
+"""
+
+
+def smallest_number(n):
+    """
+
+    :param n:
+    :return:
+    """
     smallest_number = 0
-    for i in range(n-10, n+10) :
-        reste = 0
-        for j in range(1,21) :
-            reste = reste + i % j
-        if reste == 0 and i > smallest_number :
+    for i in range(n - 10, n + 10):
+        rest = 0
+        for j in range(1, 21):
+            rest = rest + i % j
+        if rest == 0 and i > smallest_number:
             smallest_number = i
     return smallest_number
+
+
+print(smallest_number(232792560))
