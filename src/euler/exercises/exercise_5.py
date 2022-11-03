41d72109de225e609a396f21a8670fd9e78c1ce3
rlandure <raphael.landure@visiblepatient.com>
2022-11-03 14:25:47 +0100

    Add exercise 5 test

--- src/euler/exercises/exercise_5.py
+++ src/euler/exercises/exercise_5.py
@@ -1,5 +1,4 @@
-"""
-Exercise 5:
+"""Exercise 5:
 
 2520 is the smallest number that can be divided by each of the numbers from 1
 to 10 without any remainder.
@@ -7,18 +6,16 @@ to 10 without any remainder.
 What is the smallest positive number that is evenly divisible by all the numbers
 from 1 to 20 ?
 """
+
 import typer
 
 from ._registry import exercise
 
 
-@exercise(name="exercise-5")
-def exercise_5() -> None:
-    """
-    Function that returns the smallest positive number that is evenly divisible by all
+def smallest_number() -> int:
+    """Function that returns the smallest positive number that is evenly divisible by all
     numbers from 1 to 20.
 
-    :param n:
     :return: smallest positive number evenly divisible.
     """
     number = 20
@@ -26,9 +23,19 @@ def exercise_5() -> None:
         dividers = [20, 19, 18, 17, 16, 14, 13, 11]
         rest = int(sum(number % divider for divider in dividers))
         if rest == 0:
-            print(number)
-            break
+            return number
         number += 20
+
+
+@exercise(name="exercise-5")
+def exercise_5() -> None:
+    """Exercise 5: smallest positive number that is evenly divisible by all
+    numbers from 1 to 20.
+
+    :return: None.
+    """
+    result: int = smallest_number()
+    print(result)
 
 
 if __name__ == "__main__":
