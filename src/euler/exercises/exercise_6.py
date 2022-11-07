998ded348d29329bec7fae8a6c736c0ec5aa4ff6
raphael <raph.landure@gmail.com>
2022-11-07 21:48:38 +0100

    Switch to python 3.10.2; add exercise 6

--- src/euler/exercises/exercise_6.py
+++ src/euler/exercises/exercise_6.py
@@ -1,5 +1,4 @@
-"""
-Exercise 6:
+"""Exercise 6:
 
 The sum of the squares of the first ten natural numbers is, 1²+2²+...+10²=385.
 
@@ -11,6 +10,7 @@ numbers and the square of the sum is 3025−385=2640.
 Find the difference between the sum of the squares of the first one hundred
 natural numbers and the square of the sum.
 """
+
 import operator
 from functools import reduce
 
@@ -19,9 +19,27 @@ import typer
 from ._registry import exercise
 
 
+def sum_of_squares(limit: int) -> int:
+    """Computes the sum of squares until reaching the given limit.
+
+    :param limit: last square added.
+    :return: sum of squares.
+    """
+    return reduce(operator.add, [x * x for x in list(range(1, limit + 1))])
+
+
+def square_of_sum(limit: int) -> int:
+    """Computes the square of the sum until reaching the given limit.
+
+    :param limit: last number added to the sum.
+    :return: square of the sum.
+    """
+    return sum(list(range(1, limit + 1))) ** 2
+
+
 @exercise(name="exercise-6")
 def exercise_6(
-    limit: int = typer.Option(0, help="Natural number where we want to stop the sum."),
+    limit: int = typer.Option(1, help="Natural number where we want to stop the sum."),
 ) -> None:
     """
     Function that returns the difference between the sum of the squares and the
@@ -30,11 +48,7 @@ def exercise_6(
     :param limit: the natural number where we want to stop the sum.
     :return: the difference.
     """
-    sum_of_the_squares = reduce(operator.add, [x * x for x in list(range(1, limit + 1))])
-
-    square_of_the_sum = sum(list(range(1, limit + 1))) ** 2
-
-    print(square_of_the_sum - sum_of_the_squares)
+    print(square_of_sum(limit) - sum_of_squares(limit))
 
 
 if __name__ == "__main__":
---
+++ tests/euler/exercises/test_exercise_6.py
@@ -0,0 +1,8 @@
+import pytest
+
+from src.euler.exercises.exercise_6 import square_of_sum, sum_of_squares
+
+
+@pytest.mark.parametrize("limit,result", [(10, 2640), (100, 25164150)])
+def test_exercise_5(limit: int, result: int):
+    assert square_of_sum(limit) - sum_of_squares(limit) == result
